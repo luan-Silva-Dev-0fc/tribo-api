664f9b72cae2fcde94674ff82a9b13a5548f0642
luan-Silva-Dev-0fc <nascimentosilvaluan39@gmail.com>
2026-08-26 20:26:10 -0300

    fix: correct relative import paths in app.js and server.js for linux container deployment

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 const http = require('http');
 const { Server } = require('socket.io');
 const app = require('./app');
-const env = require('../src/config/env');
+const env = require('./config/env');
 const { logger } = require('./utils/logger');
 const { initializeChatSocket } = require('./sockets/chat');
 const { initializeNotificationSocket } = require('./sockets/notification');
